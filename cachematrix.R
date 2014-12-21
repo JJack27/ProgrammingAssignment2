f8814b90aaee97aa32eb7867f3f88c3766cbea7c
JJack27 <yizhouzhao27@gmail.com>
2014-12-20 23:22:13 -0800

    Finish debug part as well as the code part
    Assignment Completed

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    xn <- NULL
+    set<- function(y){
+        if(nrow(y)==ncol(y)){
+            x <<- y
+            xn<<- NULL
+        }else{print("Please check you matrix again.")}
+    }
+    get <- function() x
+    setSolve <- function(yn) xn<<- yn
+    getSolve <- function() xn
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    xn<- x$getSolve()
+    if(!is.null(xn)){
+        message("Getting inverse matrix...")
+        return(xn)
+    }
+    ninverse <- x$get()
+    xn <- solve(ninverse)
+    x$setSolve(xn)
+    xn
 }
